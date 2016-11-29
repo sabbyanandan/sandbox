1c0dfe4f0767339a1627690fb9d4071aed514d50
Marius Bogoevici <mbogoevici@pivotal.io>
2016-11-29 16:15:47 -0500

    Use pubsub binder + extra deps

--- log-sink/src/main/java/com/example/DemoApplication.java
+++ log-sink/src/main/java/com/example/DemoApplication.java
@@ -2,8 +2,11 @@ package com.example;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.stream.app.log.sink.LogSinkConfiguration;
+import org.springframework.context.annotation.Import;
 
 @SpringBootApplication
+@Import(LogSinkConfiguration.class)
 public class DemoApplication {
 
 	public static void main(String[] args) {
