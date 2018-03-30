79af8ef164e1e9962e8695c06b98650c22679b89
Sabby Anandan <sabby.anandan@gmail.com>
2018-03-30 07:23:49 -0700

    Add whitelist properties example

---
+++ channing/src/main/java/com/example/channing/ChanningAppPropertiesOne.java
@@ -0,0 +1,33 @@
+package com.example.channing;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+
+@ConfigurationProperties
+public class ChanningAppPropertiesOne {
+
+    /**
+     * The first name of the person. 
+     */
+    private String firstName;
+    /**
+     * The last name of the person.
+     */
+    private String lastName;
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+}
---
+++ channing/src/main/java/com/example/channing/ChanningAppPropertiesTwo.java
@@ -0,0 +1,33 @@
+package com.example.channing;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+
+@ConfigurationProperties
+public class ChanningAppPropertiesTwo {
+
+    /**
+     * The person's middle name.
+     */
+    private String middleName;
+    /**
+     * The date of birth.
+     */
+    private String birthdate;
+
+    public String getMiddleName() {
+        return middleName;
+    }
+
+    public void setMiddleName(String middleName) {
+        this.middleName = middleName;
+    }
+
+    public String getBirthdate() {
+        return birthdate;
+    }
+
+    public void setBirthdate(String birthdate) {
+        this.birthdate = birthdate;
+    }
+
+}
